00e135c8309664ccc7ff0c852ce4de335a46c4d6
VaithiDs <vaithids@MacBook-Pro-6.local>
2023-01-05 13:02:00 +0530

    Changed nonce and amount format

--- src/utils/near/index.ts
+++ src/utils/near/index.ts
@@ -18,9 +18,9 @@ export async function signNearRaw(tx: TTranx, prvKey: string) {
   const createAccountKeyPair = KeyPair.fromString(prvKey);
   await config.keyStore.setKey(config.networkId, tx.fromAddress, createAccountKeyPair);
   const createAccount = await near.account(tx.fromAddress);
-  const actions = [nearAPI.transactions.transfer(toBN(BigNumber(tx.amountValue ?? 0).toString(16)))];
+  const actions = [nearAPI.transactions.transfer(toBN(tx.amountHex ?? ""))];
   const { accessKey, publicKey } = await createAccount.findAccessKey(tx.fromAddress ?? '', actions);
-  const nonce = accessKey.nonce.add(toBN(tx.nonce));
+  const nonce = accessKey.nonce.add(toBN(1));
   const signedTx = await transactions.signTransaction(tx.toAddress ?? '', nonce, actions, bs58.decode(tx.blockHash ?? ''), near.connection.signer, createAccount.accountId, config.networkId);
   const nearApiJs = Buffer.from(signedTx[1].encode()).toString('base64');
   return nearApiJs;
